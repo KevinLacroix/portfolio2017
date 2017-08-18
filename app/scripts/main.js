d4ebe9768e27a06a09b9b16c0c6dd0e58dd754b3
Kévin Lacroix <kevin.lacroix.tr@gmail.com>
2017-08-18 14:53:17 +0200

    init project

---
+++ app/scripts/main.js
@@ -0,0 +1,242 @@
+
+var Portfolio = typeof Portfolio === 'undefined' ? {} : Portfolio;
+
+var winW = $(window).width(),
+    winH = $(window).height(),
+    docW = $(document).width(),
+    docH = $(document).height(),
+    is = {
+      desktop: (winW >= 993) ? true : false,
+      tablet:  (winW >= 545 && winW <= 992) ? true : false,
+      mobile:  (winW <= 544) ? true : false
+    };
+
+
+Portfolio = {
+
+  reSize: function () {
+    var id;
+    $(window).resize(function() {
+      clearTimeout(id);
+      id = setTimeout(doneResizing, 500);
+    });
+
+    function doneResizing(){
+      Portfolio.loadReSize();
+    }
+  },
+
+  loadReSize: function () {
+
+    winW = $(window).width();
+    winH = $(window).height();
+    docW = $(document).width();
+    docH = $(document).height();
+
+    var is = {
+      desktop: (winW >= 993) ? true : false,
+      tablet:  (winW >= 545 && winW <= 992) ? true : false,
+      mobile:  (winW <= 544) ? true : false
+    };
+
+    if ( (is.mobile || is.tablet) ) {
+      $('body').addClass('mobile');
+    } else {
+      $('body').removeClass('mobile');
+    }
+
+  },
+
+  // PARALLAX WITH CLOUDS
+
+  moveClouds: function() {
+    var $bigHeader = $('#js-big-header');
+    var movementStrength2 = 4;
+    var movementStrength3 = 6;
+    var height2 = movementStrength2 / $(window).height();
+    var height3 = movementStrength3 / $(window).height();
+    var width2 = movementStrength2 * 5 / $(window).width();
+    var width3 = movementStrength3 * 5 / $(window).width();
+    $bigHeader.mousemove(function(e) {
+      var pageX = e.pageX - ($(window).width() / 2);
+      var pageY = e.pageY - ($(window).height() / 2);
+      var newvalueX2 = 50 - (width2 * pageX);
+      var newvalueX3 = 50 - (width3 * pageX);
+      var newvalueY2 = 100 - (height2 * pageY);
+      var newvalueY3 = 100 - (height3 * pageY);
+      $('.header-big-bg2').css('background-position', newvalueX2+'% '+newvalueY2+'%');
+      $('.header-big-bg3').css('background-position', newvalueX3+'% '+newvalueY3+'%');
+    });
+  },
+
+
+  // STICKY NAV ON SCROLL
+
+  stickyNav: function() {
+    var $body = $('body');
+    var $winScrollTop = $(window).scrollTop();
+    if ($winScrollTop > 20) {
+      $('.header-nav').addClass('is-small');
+    } else {
+      $('.header-nav').removeClass('is-small');
+    }
+  },
+
+  bindSticky: function() {
+    $(window).bind('scroll', Portfolio.stickyNav);
+  },
+
+  activeNavLinks: function() {
+
+    var waypoints = $('.waypoint');
+    waypoints.waypoint(function(direction){
+      if (direction === "down") {
+        var matchingLink = $('a[href="#' + this.element.id + '"]');
+        $('.header-nav').find('.header-nav-item').removeClass('is-active');
+        matchingLink.parent('.header-nav-item').addClass('is-active');
+      }
+    }, {offset: '200px'});
+
+    waypoints.waypoint(function(direction){
+      if (direction === "up") {
+        var matchingLink = $('a[href="#' + this.element.id + '"]');
+        $('.header-nav').find('.header-nav-item').removeClass('is-active');
+        matchingLink.parent('.header-nav-item').addClass('is-active');
+      }
+    }, {offset: '-500px'});
+  },
+
+
+  // SMOOTH SCROLL FUNCTION
+
+  smoothScroll: function() {
+    $('[data-href').click(function() {
+      var button = $(this);
+      var anchor = button.data('href');
+      Portfolio.scrollTo(anchor);
+      return false;
+    });
+  },
+
+  scrollTo: function(href) {
+    $('html, body').animate({
+      scrollTop: $(href).offset().top - 70
+    }, 'slow', function(){
+      window.location.hash = href;
+    });
+  },
+
+  cogRotate: function() {
+    $(window).scroll(function() {
+      var wScroll = $(this).scrollTop();
+      $('.cog-item').css({
+        'transform': 'rotate(' + wScroll / 5 + 'deg)'
+      });
+      $('.cog-item-opp').css({
+        'transform': 'rotate(-' + wScroll / 5 + 'deg)'
+      });
+    });
+  }
+
+};
+
+
+$(document).ready(function() {
+
+  Portfolio.reSize();
+  Portfolio.bindSticky();
+  Portfolio.smoothScroll();
+
+  // Gears FAQ
+  if ( $('.timeline-cogs').length ) {
+    Portfolio.cogRotate();
+  }
+
+  if ( is.desktop ) {
+
+    Portfolio.activeNavLinks();
+
+    // ANIMATED HEADER BACKGROUNDS
+
+    if ($('#js-big-header').length) {
+      animatedHeaderBackgrounds();
+    }
+
+    // MOVE CLOUDS
+
+    if ($('#js-big-header').length) {
+      Portfolio.moveClouds();
+    }
+
+    // PLUGIN FOR DIRECTION-AWARE HOVER EFFECT
+
+    if ($('.projects-section').length) {
+      $('.project-item').hoverdir({hoverElem: '.project-caption'});
+    }
+
+    if ( $('.tagline-animated').length ) {
+
+      $('.typed-insertion1').typed({
+        stringsElement: $('.list-keywords1'),
+        typeSpeed: 50,
+        startDelay: 1000,
+        loop: true,
+        loopCount: 3,
+        backSpeed: 30,
+        shuffle: false,
+        backDelay: 3000
+      });
+
+      $('.typed-insertion2').typed({
+        stringsElement: $('.list-keywords2'),
+        typeSpeed: 50,
+        startDelay: 1000,
+        loop: true,
+        loopCount: 3,
+        backSpeed: 30,
+        shuffle: false,
+        backDelay: 3000
+      });
+
+    }
+
+  }
+
+  // Modal
+
+  if ( $('.project-link').length ) {
+    $('#trigger-modal1').animatedModal({
+      modalTarget:'animatedModal1',
+      animatedIn:'zoomIn',
+      animatedOut:'bounceOut',
+      color:'rgba(241,96,88,0.96)'
+    });
+
+    $('#trigger-modal2').animatedModal({
+      modalTarget:'animatedModal2',
+      animatedIn:'lightSpeedIn',
+      animatedOut:'bounceOutDown',
+      color:'rgba(241,96,88,0.96)'
+    });
+
+    $('#trigger-modal3').animatedModal({
+      modalTarget:'animatedModal3',
+      animatedIn:'bounceIn',
+      animatedOut:'bounceOut',
+      color:'rgba(241,96,88,0.96)'
+    });
+
+    $('#trigger-modal4').animatedModal({
+      modalTarget:'animatedModal4',
+      animatedIn:'zoomIn',
+      animatedOut:'bounceOut',
+      color:'rgba(241,96,88,0.96)'
+    });
+  }
+
+});
+
+(function($) {
+
+})(jQuery);
+
---
+++ app/scripts/scrollReveal.js
@@ -0,0 +1,133 @@
+$(function() {
+
+  var $win              = $(window),
+      $win_height       = $(window).height(),
+      noMoreFades       = false,
+      windowPercentage  = ($(window).height() / 1.1),
+      isTouch           = (Modernizr.touch),
+      lastInstance      = $('.page-footer').find('.js-scroll-reveal');
+
+  // No animations for mobiles
+  if (isTouch) {
+    $('.js-scroll-reveal').addClass('was-animated');
+  }
+
+  $win.on('scroll', scrollReveal);
+
+  function scrollReveal() {
+    var scrolled = $win.scrollTop();
+
+    if (lastInstance.hasClass('was-animated')) {
+      noMoreFades = true;
+    }
+
+    $('.js-scroll-reveal:not(.was-animated)').each(function () {
+      var $this     = $(this),
+          offsetTop = $this.offset().top;
+
+      if (scrolled + windowPercentage > offsetTop || $win_height > offsetTop) {
+
+        // Anim pulse
+        if ($this.hasClass('js-pulse')) {
+          $this.addClass('was-animated anim-pulse');
+        }
+        // Anim flipInX
+        else if ($this.hasClass('js-flipInX')) {
+          $this.addClass('was-animated anim-flipInX');
+        }
+        // Anim fadeInUp
+        else if ($this.hasClass('js-fadeInUp')) {
+          $this.addClass('was-animated anim-fadeInUp');
+        }
+        // Anim bounceIn
+        else if ($this.hasClass('js-bounceIn')) {
+          $this.addClass('was-animated anim-bounceIn');
+        }
+        // Anim bounceInLeft
+        else if ($this.hasClass('js-bounceInLeft')) {
+          $this.addClass('was-animated anim-bounceInLeft');
+        }
+        // Anim bounceInRight
+        else if ($this.hasClass('js-bounceInRight')) {
+          $this.addClass('was-animated anim-bounceInRight');
+        }
+
+        // Anim Title Decoration
+
+        // Anim decoTitle
+        else if ($this.hasClass('js-decoTitle')) {
+          $this.addClass('anim-decoTitle');
+        }
+
+        // Second
+
+        // Anim flipInX
+        else if ($this.hasClass('js-flipInX-second')) {
+          window.setTimeout(function(){
+            $this.addClass('was-animated anim-flipInX');
+          }, 400);
+        }
+        // Anim fadeInUp
+        else if ($this.hasClass('js-fadeInUp-second')) {
+          window.setTimeout(function(){
+            $this.addClass('was-animated anim-fadeInUp');
+          }, 400);
+        }
+        // Anim bounceInLeft
+        else if ($this.hasClass('js-bounceInLeft-second')) {
+          window.setTimeout(function(){
+            $this.addClass('was-animated anim-bounceInLeft');
+          }, 400);
+        }
+        // Anim bounceInRight
+        else if ($this.hasClass('js-bounceInRight-second')) {
+          window.setTimeout(function(){
+            $this.addClass('was-animated anim-bounceInRight');
+          }, 400);
+        }
+
+        // Third
+
+        // Anim flipInX
+        else if ($this.hasClass('js-flipInX-third')) {
+          window.setTimeout(function(){
+            $this.addClass('was-animated anim-flipInX');
+          }, 800);
+        }
+        // Anim fadeInUp
+        else if ($this.hasClass('js-fadeInUp-third')) {
+          window.setTimeout(function(){
+            $this.addClass('was-animated anim-fadeInUp');
+          }, 800);
+        }
+
+        // FOURTH
+
+        // Anim flipInX
+        else if ($this.hasClass('js-flipInX-fourth')) {
+          window.setTimeout(function(){
+            $this.addClass('was-animated anim-flipInX');
+          }, 1200);
+        }
+
+        // SKILLBAR WIDTH ON SKILL PAGE
+
+        else if ($this.hasClass('skill-bar-inner')) {
+          //setTimeout(function() {
+            $this.css('width', $this.data('percent'));
+            $this.addClass('was-animated');
+          // }, 50);
+        }
+
+        // Default
+
+        // Anim flipInX
+        else {
+          $(this).addClass('was-animated anim-flipInX');
+        }
+      }
+    });
+  }
+
+  scrollReveal();
+});
